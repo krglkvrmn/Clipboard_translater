8d9bae82d3246499958c09734913995b5c82d944
Kruglikov Roman <kruglikov1911@mail.ru>
2020-09-10 01:54:37 +0300

    Added comments, repeated code moved to separated function, renamed variables

--- translater.py
+++ translater.py
@@ -1,36 +1,56 @@
-import requests
-import clipboard as clip
 from collections import namedtuple
 from bs4 import BeautifulSoup
+import clipboard as clip
+import requests
 
+
+# Defining basic structure for translation representation.
 Translation = namedtuple('Translation', ['word', 'transcription', 'translation'])
+TARGET_URL = 'https://wooordhunt.ru/word/postsearch'
 
-def translate_from_clip():
-    form_url = 'https://wooordhunt.ru/word/postsearch'
+
+def _translate(word: str) -> Translation:
+    """
+    Translate word.
+
+    Input:
+            1. Word to translate.
+
+    Output:
+            1. Translation object, which has translation, word and
+            transcription attributes.
+    """
+
+    response = requests.post(TARGET_URL, data={"word": word})
+    try:
+        # Encoding troubles with transcription in case of plain text
+        soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
+        translation = soup.find('span', class_='t_inline_en').text
+        transcription = soup.find('span', class_='transcription').text
+    # Translation was not found
+    except AttributeError as exc:
+        print(exc)
+        return
+    return Translation(word, transcription, translation)
+
+
+def translate_from_clip() -> Translation:
+    """
+    Translate word in clipboard.
+
+    Output:
+            1. Translation object, which has translation, word and
+            transcription attributes.
+    """
+    # Get text from clipboard
     clip_text = clip.paste()
-    response = requests.post(form_url, data={"word": clip_text})
-    try:
-        soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
-        word = clip_text
-        translation = soup.find('span', class_='t_inline_en').text
-        transcription = soup.find('span', class_='transcription').text
-    except AttributeError as exc:
-        print(exc)
-        return
-    return Translation(word, transcription, translation)
+    return _translate(clip_text)
 
-def translate_from_str(text):
-    form_url = 'https://wooordhunt.ru/word/postsearch'
-    response = requests.post(form_url, data={"word": text})
-    try:
-        soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
-        word = text
-        translation = soup.find('span', class_='t_inline_en').text
-        transcription = soup.find('span', class_='transcription').text
-    except AttributeError as exc:
-        print(exc)
-        return
-    return Translation(word, transcription, translation)
+
+def translate_from_str(word: str) -> Translation:
+    return _translate(word)
+translate_from_str.__doc__ = _translate.__doc__
+
 
 if __name__ == '__main__':
     print(translate_from_clip())
