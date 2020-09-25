ec2efe2f1b8fb8cebd336a6b56a9dbfa39c8a5a4
Kruglikov Roman <kruglikov1911@mail.ru>
2020-09-25 16:52:10 +0300

    GUI language select added (no logic)

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
@@ -29,6 +30,7 @@ KV_FILE = """
     query_input: qi
     trans_inst: ti
     hotkey: sw
+    sett_man: sm
 
     orientation: 'vertical'
     QueryInputField:
@@ -42,6 +44,21 @@ KV_FILE = """
         size_hint: 1,0.3
         Switch:
             id: sw
+        SettingsManager:
+            id: sm
+            orientation: 'horizontal'
+            lang_select: lang_sel
+            trans_select: trans_sel
+            call_ls: call_ls
+            call_tr: call_tr
+            DropdownCallButton:
+                id: call_ls
+            LanguageSelectField:
+                id: lang_sel
+            DropdownCallButton:
+                id: call_tr
+            TranslaterSelectField:
+                id: trans_sel
 
 <QueryInputField>:
     halign: 'center'
@@ -56,7 +73,16 @@ KV_FILE = """
     text_size: self.width, None
     valign: 'center'
     halign: 'center'
+
+<DropdownCallButton>:
+    size_hint: 1,0.4
+    pos_hint: {'top':0.5}
 """
+
+AVAILABLE_LANGS = ["English", "Deutsch", "Guess"]
+AVAILABLE_TRANSLATERS = {"English": ["wooordhunt", "yandex"],
+                         "Deutsch": ["yandex"],
+                         "Guess": [""]}
 
 
 Config.set('graphics', 'resizable', '0')
@@ -71,6 +97,8 @@ class MainWindow(BoxLayout):
     trans_inst = ObjectProperty(None)
     # Instance of hotkey on/off switch.
     hotkey = ObjectProperty(None)
+    # Settings manager instance
+    sett_man = ObjectProperty(None)
 
     def _check_hotkey(self, dt):
         """
@@ -105,6 +133,8 @@ class MainWindow(BoxLayout):
         notification.notify(message=translation, title=f'Перевод "{word}"', timeout=6)
 
 
+
+
 class QueryInputField(TextInput):
     def on_enter(instance, value):
         """
@@ -125,10 +155,61 @@ class TranslationField(Label):
     pass
 
 
+class SettingsManager(BoxLayout):
+    # Instance of language select dropdown menu
+    lang_select = ObjectProperty(None)
+    # Instance of translater select dropdown menu
+    trans_select = ObjectProperty(None)
+    # Main button for language dropdown
+    call_ls = ObjectProperty(None)
+    # Main button for translater dropdown
+    call_tr = ObjectProperty(None)
+
+    def setup_language_dropdown(self):
+        def __on_select(inst, data):
+            self.call_ls.text = data
+            self.setup_trans_dropdown()
+        dropdown = self.lang_select              # Alias
+        self.call_ls.text = "English"
+        for lang in AVAILABLE_LANGS:
+            btn = Button(text=lang, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+        self.call_ls.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=__on_select)
+
+    def setup_trans_dropdown(self):
+        current_lang = self.call_ls.text
+        dropdown = self.trans_select
+        dropdown.clear_widgets()
+        tr_list =  AVAILABLE_TRANSLATERS[current_lang]
+        self.call_tr.text = tr_list[0]
+        for trans in tr_list:
+            btn = Button(text=trans, size_hint_y=None, height=44)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+        self.call_tr.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda inst, data: setattr(self.call_tr, "text", data))
+
+
+class LanguageSelectField(DropDown):
+    pass
+
+
+class TranslaterSelectField(DropDown):
+    pass
+
+
+class DropdownCallButton(Button):
+    pass
+
+
 class TranslaterApp(App):
     def build(self):
         Builder.load_string(KV_FILE)
         main = MainWindow()
+        main.sett_man.setup_language_dropdown()
+        main.sett_man.setup_trans_dropdown()
         # Check if hotkey was pressed.
         Clock.schedule_interval(main._check_hotkey, 1/60)
         return main
